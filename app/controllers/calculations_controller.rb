47bb1124e7611c8af1254bb4092e62e32edded88
csprayregen <csprayregen@users.noreply.github.com>
2017-04-25 15:16:13 -0500

    quick update at whole foods

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,16 +1,23 @@
 class CalculationsController < ApplicationController
 
+  def square_results
+    @user_num = params[:user_num].to_f
+    render("calculations/square_results.html.erb")
+  end
+
+  def square_form
+    render("calculations/square_form.html.erb")
+  end
+
   def flexible_square
     @user_num = params["number"].to_f
     #Parameters: {"number"=>"8"}
-
-
     render("calculations/flexible_square.html.erb")
   end
 
   def flexible_square_root
     @user_num = params["number"].to_f
-    @square_root = @user_num ** .5
+    ##deal with square root issue
 
 
     render("calculations/flexible_square_root.html.erb")
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   get("/flexible/square_root/:number", {:controller => "calculations", :action => "flexible_square_root"})
 
+  get("/square/new", {:controller =>"calculations", :action => "square_form"})
+
+  get("/square/results", {:controller =>"calculations", :action => "square_results"})
 
 end
