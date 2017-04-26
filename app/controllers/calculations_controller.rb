1ab9939231a4236627a719161b94fe0a0664b37b
csprayregen <csprayregen@users.noreply.github.com>
2017-04-26 09:57:03 -0500

    Revert "quick update at whole foods"
    This reverts commit 47bb1124e7611c8af1254bb4092e62e32edded88.

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,23 +1,16 @@
 class CalculationsController < ApplicationController
 
-  def square_results
-    @user_num = params[:user_num].to_f
-    render("calculations/square_results.html.erb")
-  end
-
-  def square_form
-    render("calculations/square_form.html.erb")
-  end
-
   def flexible_square
     @user_num = params["number"].to_f
     #Parameters: {"number"=>"8"}
+
+
     render("calculations/flexible_square.html.erb")
   end
 
   def flexible_square_root
     @user_num = params["number"].to_f
-    ##deal with square root issue
+    @square_root = @user_num ** .5
 
 
     render("calculations/flexible_square_root.html.erb")
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,5 @@ Rails.application.routes.draw do
 
   get("/flexible/square_root/:number", {:controller => "calculations", :action => "flexible_square_root"})
 
-  get("/square/new", {:controller =>"calculations", :action => "square_form"})
-
-  get("/square/results", {:controller =>"calculations", :action => "square_results"})
 
 end
